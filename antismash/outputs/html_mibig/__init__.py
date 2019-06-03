d7a4872a8c89dcb65abcfe024be474fadf65bac5
Satria A Kautsar <satriaphd@gmail.com>
2019-06-03 15:11:25 +0200

    initial commit: --mibig-mode and --mibig-json <json-file-path>

---
+++ antismash/outputs/html_mibig/__init__.py
@@ -0,0 +1,80 @@
+# License: GNU Affero General Public License v3 or later
+# A copy of GNU AGPL v3 should have been included in this software package in LICENSE.txt.
+
+"""HTML output format module
+
+"""
+
+import glob
+import os
+import shutil
+from typing import Dict, List, Optional
+
+from antismash.common import path
+from antismash.common.module_results import ModuleResults
+from antismash.common.secmet import Record
+from antismash.config import ConfigType
+from antismash.config.args import ModuleArgs
+from antismash.outputs.html_mibig.generator import generate_webpage
+
+NAME = "html_mibig"
+SHORT_DESCRIPTION = "HTML output for mibig-mode"
+
+
+def check_options(_options: ConfigType) -> List[str]:
+    """ Check options, but none to check """
+    return []
+
+
+def is_enable(_options: ConfigType) -> bool:
+    """ Is the HMTL module enabled (currently always enabled) """
+    return True  # TODO: add an arg to disable
+
+
+def write(records: List[Record], results: List[Dict[str, ModuleResults]],
+          options: ConfigType) -> None:
+    """ Writes all results to a webpage, where applicable. Writes to options.output_dir
+
+        Arguments:
+            records: the list of Records for which results exist
+            results: a list of dictionaries containing all module results for records
+            options: antismash config object
+
+        Returns:
+            None
+    """
+    output_dir = options.output_dir
+
+    copy_template_dir('css', output_dir, pattern="mibig.css")
+    copy_template_dir('js', output_dir)
+    copy_template_dir('images', output_dir)
+
+    # Generate structure images for records obtained from BioSQL
+    generate_webpage(records, results, options)
+
+
+def copy_template_dir(template: str, output_dir: str, pattern: Optional[str] = None) -> None:
+    """ Copy files from a template directory to the output directory, removes
+        any existing directory first. If pattern is supplied, only files within
+        the template directory that match the template will be copied.
+
+        Arguments:
+            template: the source directory
+            output_dir: the target directory
+            pattern: a pattern to restrict to, if given
+
+        Returns:
+            None
+    """
+    target_dir = os.path.join(output_dir, template)
+    if os.path.exists(target_dir):
+        shutil.rmtree(target_dir)
+    if pattern:
+        os.makedirs(target_dir)
+        for filename in glob.glob(path.get_full_path(__file__, template, pattern)):
+            if os.path.isdir(filename):
+                shutil.copytree(filename, target_dir)
+            else:
+                shutil.copy2(filename, target_dir)
+    else:
+        shutil.copytree(path.get_full_path(__file__, template), target_dir)
