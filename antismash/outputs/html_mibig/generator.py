aa102e0cb98fa0fe0f90bf366cf23768f81b0873
Satria A Kautsar <satriaphd@gmail.com>
2019-06-03 15:11:25 +0200

    initial commit: --mibig-mode and --mibig-json <json-file-path>

--- antismash/outputs/html_mibig/generator.py
+++ antismash/outputs/html_mibig/generator.py
@@ -6,6 +6,7 @@
 import json
 import string
 import os
+<<<<<<< HEAD
 import re
 from typing import Any, Dict, List, Tuple, Union, cast
 
@@ -23,6 +24,18 @@ from antismash.outputs.html.generator import (
     generate_searchgtr_htmls,
     write_regions_js,
 )
+=======
+from typing import Any, Dict, List, Tuple, Union
+
+from antismash.common import path, module_results
+from antismash.common.html_renderer import FileTemplate, HTMLSections, docs_link
+from antismash.common.layers import RecordLayer, OptionsLayer
+from antismash.common.secmet import Record
+from antismash.common.json import JSONOrf
+from antismash.config import ConfigType
+from antismash.outputs.html_mibig import js
+from antismash.custom_typing import AntismashModule
+>>>>>>> f92b796f (initial commit: --mibig-mode and --mibig-json <json-file-path>)
 
 
 def build_json_data(records: List[Record], results: List[Dict[str, module_results.ModuleResults]],
@@ -48,6 +61,7 @@ def build_json_data(records: List[Record], results: List[Dict[str, module_result
 
     for i, record in enumerate(records):
         json_record = js_records[i]
+<<<<<<< HEAD
         # replace antismash cds_detail with mibig's one
         try:
             cds_annotations = results[i]["antismash.detection.mibig"].data.cluster.genes.annotations
@@ -60,6 +74,11 @@ def build_json_data(records: List[Record], results: List[Dict[str, module_result
             handlers = find_plugins_for_cluster(get_all_modules(), json_region)
             if nrps_pks_domains not in handlers and nrps_pks_domains.domain_drawing.has_domain_details(region):
                 handlers.append(cast(AntismashModule, nrps_pks_domains))
+=======
+        json_record['seq_id'] = "".join(char for char in json_record['seq_id'] if char in string.printable)
+        for region, json_region in zip(record.get_regions(), json_record['regions']):
+            handlers = find_plugins_for_cluster(get_all_modules(), json_region)
+>>>>>>> f92b796f (initial commit: --mibig-mode and --mibig-json <json-file-path>)
             for handler in handlers:
                 # if there's no results for the module, don't let it try
                 if handler.__name__ not in results[i]:
@@ -72,6 +91,28 @@ def build_json_data(records: List[Record], results: List[Dict[str, module_result
     return js_records, js_domains
 
 
+<<<<<<< HEAD
+=======
+def write_regions_js(records: List[Dict[str, Any]], output_dir: str,
+                     js_domains: List[Dict[str, Any]]) -> None:
+    """ Writes out the cluster and domain JSONs to file for the javascript sections
+        of code"""
+    with open(os.path.join(output_dir, 'regions.js'), 'w') as handle:
+        handle.write("var recordData = %s;\n" % json.dumps(records, indent=4))
+        regions = {"order": []}  # type: Dict[str, Any]
+        for record in records:
+            for region in record['regions']:
+                regions[region['anchor']] = region
+                regions['order'].append(region['anchor'])
+        handle.write('var all_regions = %s;\n' % json.dumps(regions, indent=4))
+
+        clustered_domains = {}
+        for region in js_domains:
+            clustered_domains[region['id']] = region
+        handle.write('var details_data = %s;\n' % json.dumps(clustered_domains, indent=4))
+
+
+>>>>>>> f92b796f (initial commit: --mibig-mode and --mibig-json <json-file-path>)
 def generate_html_sections(records: List[RecordLayer], results: Dict[str, Dict[str, module_results.ModuleResults]],
                            options: ConfigType) -> Dict[str, Dict[int, List[HTMLSections]]]:
     """ Generates a mapping of record->region->HTMLSections for each record, region and module
@@ -92,6 +133,7 @@ def generate_html_sections(records: List[RecordLayer], results: Dict[str, Dict[s
         record_details = {}
         record_result = results[record.id]
         for region in record.regions:
+<<<<<<< HEAD
             # work around mibig module not creating protoclusters with the expected types
             assert len(region.subregions) == 1 and region.subregions[0].tool == "mibig"
             if nrps_pks_domains.domain_drawing.has_domain_details(region.region_feature):
@@ -104,6 +146,19 @@ def generate_html_sections(records: List[RecordLayer], results: Dict[str, Dict[s
                     if handler_results is None:
                         continue
                     sections.append(handler.generate_html(region, handler_results, record, options))
+=======
+            sections = []
+            for handler in region.handlers:
+                if handler.will_handle(region.products):
+                    handler_results = record_result.get(handler.__name__)
+                    if handler_results is None:
+                        continue
+                    html = handler.generate_html(region, handler_results, record, options)
+                    if isinstance(html, list):
+                        sections.extend(html)
+                    else:
+                        sections.append(html)
+>>>>>>> f92b796f (initial commit: --mibig-mode and --mibig-json <json-file-path>)
             record_details[region.get_region_number()] = sections
         details[record.id] = record_details
     return details
@@ -131,6 +186,7 @@ def generate_webpage(records: List[Record], results: List[Dict[str, module_resul
                 record_layers_without_regions.append(RecordLayer(record, None, options_layer))
             results_by_record_id[record.id] = record_results
 
+<<<<<<< HEAD
         regions_written = sum(len(record.get_regions()) for record in records)
         job_id = os.path.basename(options.output_dir)
 
@@ -138,6 +194,13 @@ def generate_webpage(records: List[Record], results: List[Dict[str, module_resul
         annotation_filename = "{}.json".format(mibig_id)
         page_title = mibig_id
 
+=======
+        page_title = "MIBiG Repository"
+
+        regions_written = sum(len(record.get_regions()) for record in records)
+        job_id = os.path.basename(options.output_dir)
+
+>>>>>>> f92b796f (initial commit: --mibig-mode and --mibig-json <json-file-path>)
         html_sections = generate_html_sections(record_layers_with_regions, results_by_record_id, options)
 
         svg_tooltip = ("Shows the layout of the region, marking coding sequences and areas of interest. "
@@ -153,6 +216,7 @@ def generate_webpage(records: List[Record], results: List[Dict[str, module_resul
                               results_by_record_id=results_by_record_id,
                               config=options, job_id=job_id, page_title=page_title,
                               records_without_regions=record_layers_without_regions,
+<<<<<<< HEAD
                               svg_tooltip=svg_tooltip,
                               annotation_filename=annotation_filename, mibig_id=mibig_id)
         result_file.write(aux)
@@ -187,3 +251,54 @@ def update_cds_description(js_record, annotations):
                         annotation = None
             additional_annots = "<br>added<br>"
             js_cds["description"] = re.sub("(\(total: (.+) nt\)<br>)", r"\1{}".format(template.render(annotation=annotation)), str(js_cds["description"]))
+=======
+                              svg_tooltip=svg_tooltip)
+        result_file.write(aux)
+
+
+def find_plugins_for_cluster(plugins: List[AntismashModule], cluster: Dict[str, Any]) -> List[AntismashModule]:
+    "Find a specific plugin responsible for a given gene cluster type"
+    products = cluster['products']
+    handlers = []
+    for plugin in plugins:
+        if not hasattr(plugin, 'will_handle'):
+            continue
+        if plugin.will_handle(products):
+            handlers.append(plugin)
+    return handlers
+
+
+def load_searchgtr_search_form_template() -> List[str]:
+    """ for SEARCHGTR HTML files, load search form template """
+    with open(path.get_full_path(__file__, "templates", "searchgtr_form.html"), "r") as handle:
+        template = handle.read().replace("\r", "\n")
+    return template.split("FASTASEQUENCE")
+
+
+def generate_searchgtr_htmls(records: List[Record], options: ConfigType) -> None:
+    """ Generate lists of COGs that are glycosyltransferases or transporters """
+    gtrcoglist = ['SMCOG1045', 'SMCOG1062', 'SMCOG1102']
+    searchgtrformtemplateparts = load_searchgtr_search_form_template()
+    # TODO store somewhere sane
+    js.searchgtr_links = {}
+    for record in records:
+        for feature in record.get_cds_features():
+            smcog_functions = feature.gene_functions.get_by_tool("smcogs")
+            if not smcog_functions:
+                continue
+            smcog = smcog_functions[0].description.split(":")[0]
+            if smcog not in gtrcoglist:
+                continue
+            html_dir = os.path.join(options.output_dir, "html")
+            if not os.path.exists(html_dir):
+                os.mkdir(html_dir)
+            formfileloc = os.path.join(html_dir, feature.get_name() + "_searchgtr.html")
+            link_loc = os.path.join("html", feature.get_name() + "_searchgtr.html")
+            gene_id = feature.get_name()
+            js.searchgtr_links[record.id + "_" + gene_id] = link_loc
+            with open(formfileloc, "w") as formfile:
+                specificformtemplate = searchgtrformtemplateparts[0].replace("GlycTr", gene_id)
+                formfile.write(specificformtemplate)
+                formfile.write("%s\n%s" % (gene_id, feature.translation))
+                formfile.write(searchgtrformtemplateparts[1])
+>>>>>>> f92b796f (initial commit: --mibig-mode and --mibig-json <json-file-path>)
