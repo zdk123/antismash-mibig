8374025e51925cff1f48008d5f7881ccbb527407
Satria A Kautsar <satriaphd@gmail.com>
2019-06-27 08:06:49 +0200

    run_mibig now save log into a text file

--- run_mibig.py
+++ run_mibig.py
@@ -8,6 +8,11 @@ from os import path
 from sys import argv
 from subprocess import call
 from shutil import rmtree
+from datetime import datetime
+
+def write_log(text, file_path):
+    with open(file_path, "a") as o:
+        o.write("[{}] {}\n".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), text))
 
 def _main():
     json_path = argv[1]
@@ -19,6 +24,8 @@ def _main():
     else:
         use_source = False
     antismash_path = path.abspath(path.dirname(__file__))
+
+    log_file_path = path.join(output_folder, "log-{}".format(datetime.now().strftime('%Y-%m-%d-%H.%M.%S')))
 
     with open(json_path, "r") as json_file:
         data = json.load(json_file)
@@ -47,10 +54,18 @@ def _main():
         reuse_success = False
         if path.exists(reusable_json_path):
             reuse_success = call(commands[:-1] + ["--reuse-results", reusable_json_path]) == 0
+            if reuse_success:
+                write_log("Successfully reused JSON file {}".format(reusable_json_path), log_file_path)
+            else:
+                write_log("Failed to reuse JSON file {}".format(reusable_json_path), log_file_path)
         if path.exists(output_path) and not reuse_success:
             # remove output path, proceed with caution!
             rmtree(output_path)
-        if reuse_success or call(commands) == 0:
+            write_log("Removed {}".format(output_path), log_file_path)
+        if not reuse_success and call(commands) == 1:
+            write_log("Failed to generate MIBiG page for {}".format(mibig_acc), log_file_path)
+        else:
+            write_log("Successfully generated MIBiG page for {}".format(mibig_acc), log_file_path)
             print("Generating antiSMASH output for {}".format(mibig_acc))
             with open(cache_json_path) as handle:
                 cached = json.load(handle   )
@@ -69,10 +84,13 @@ def _main():
                 ]
                 if not use_source:
                     commands = ["antismash"] + commands[2:]
+                reuse_as5_success = False
                 if path.exists(reusable_as5_json_path):
-                    commands.pop(-1) # remove sequence path
-                    commands.extend(["--reuse-results", reusable_as5_json_path]) # add reusable json path
-                call(commands)
+                    reuse_as5_success = call(commands[:-1] + ["--reuse-results", reusable_as5_json_path]) == 0
+                if reuse_as5_success or call(commands) == 0:
+                    write_log("Successfully generated antiSMASH5 page for {}".format(mibig_acc), log_file_path)
+                else:
+                    write_log("Failed to generate antiSMASH5 page for {}".format(mibig_acc), log_file_path)
             elif "Fungi" in taxonomy:
                 commands = [
                     "python",
@@ -86,14 +104,19 @@ def _main():
                 ]
                 if not use_source:
                     commands = ["antismash"] + commands[2:]
+                reuse_as5_success = False
                 if path.exists(reusable_as5_json_path):
-                    commands.pop(-1) # remove sequence path
-                    commands.extend(["--reuse-results", reusable_as5_json_path]) # add reusable json path
-                call(commands)
+                    reuse_as5_success = call(commands[:-1] + ["--reuse-results", reusable_as5_json_path]) == 0
+                if reuse_as5_success or call(commands) == 0:
+                    write_log("Successfully generated antiSMASH5 page for {}".format(mibig_acc), log_file_path)
+                else:
+                    write_log("Failed to generate antiSMASH5 page for {}".format(mibig_acc), log_file_path)
             elif "Viridiplantae" in taxonomy:
                 "Plant BGC is temporarily not supported"
+                write_log("Plant BGCs {}".format(mibig_acc), log_file_path)
             else:
                 "Taxon is not supported (yet)"
+                write_log("Unrecognizable taxons {} ({})".format(mibig_acc, ":".join(taxonomy)), log_file_path)
 
 if __name__ == "__main__":
     _main()
